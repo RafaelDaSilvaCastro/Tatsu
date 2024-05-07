b2436e8fd4d4424e8969a55dc89b3bcf4417f521
Lucas Alano <lucasdeoliveiraalano@gmail.com>
2024-05-06 23:38:55 -0300

    Commit CSS Header
    Atualização css do Header

--- frontend/src/assets/Component/Header/Header.jsx
+++ frontend/src/assets/Component/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Header.css'; // Estilo CSS para o cabeçalho
 
-const Header = ({ userPhoto, siteLogo }) => {
+import logo from './logo.png'
+import userPhoto from './userPhoto.png'
+
+const Header = () => {
+
   return (
     <header className="header">
-      <div className="menu-toggle">&#332c2c66;</div>
-      <div className="logo">
-        <img src={siteLogo} alt="Logo do site" />
-      </div>
-      <div className="user-photo">
-        <img src={userPhoto} alt="Foto do usuário" />
+      <div className='container'> 
+        <div classname='menu-toggle'>
+            <span className='linha'></span>
+            <span className='linha'></span>
+            <span className='linha'></span>
+        </div>
+        <div className="logo">
+          <img src={logo} alt="Logo do site" />
+        </div>
+        <div className="user-photo">
+          <img src={userPhoto} alt="Foto do usuário" />
+        </div>
       </div>
     </header>
   );
