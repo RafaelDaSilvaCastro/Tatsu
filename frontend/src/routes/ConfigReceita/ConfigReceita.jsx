16f0b0008e72903ae77ad7be11c1fcb28c538b81
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2024-05-16 20:20:00 -0300

    Ajustando css dos input

--- frontend/src/routes/ConfigReceita/ConfigReceita.jsx
+++ frontend/src/routes/ConfigReceita/ConfigReceita.jsx
@@ -7,17 +7,17 @@ import Header from "../../assets/Component/Header/Header";
 
 
 const options = [
-  { value: '1', label: '1 Pessoa' },
-  { value: '2', label: '2 Pessoa' },
-  { value: '3', label: '3 Pessoa' },
-  { value: '4', label: '4 Pessoa' },
+  { value: 'option1', label: '1 Pessoa' },
+  { value: 'option2', label: '2 Pessoa' },
+  { value: 'option3', label: '3 Pessoa' },
+  { value: 'option3', label: '4 Pessoa' },
 ];
 
 
-export default function ConfigReceita(){
+export default function ConfigReceita() {
   const [ingredient, setIngredient] = useState('');
   const [ingredientsList, setIngredientsList] = useState([]);
-  const [activeButton, setActiveButton] = useState(null); 
+  const [activeButton, setActiveButton] = useState(null);
 
   const handleInputChange = (event) => {
     setIngredient(event.target.value);
@@ -38,19 +38,19 @@ export default function ConfigReceita(){
   };
 
   const handleButtonClick = (buttonIndex) => {
-    setActiveButton(buttonIndex); 
+    setActiveButton(buttonIndex);
   };
 
   const [selectedOption, setSelectedOption] = useState(options[0].value); // Initial selected value
 
   const handleChange = (event) => {
     setSelectedOption(event.target.value);
-  };  
+  };
 
   return (
-    
+
     <div className="ingredient-adder">
-      <Header/>
+      <Header />
       <h1>Lista de ingredientes</h1>
       <ul className="ingredients-list">
         {ingredientsList.map((ingredient) => (
@@ -61,50 +61,54 @@ export default function ConfigReceita(){
             </button>
           </li>
         ))}
-      </ul>   
+      </ul>
       <div className="ingredient-input">
-        <input
+        <input className='teste'
           type="text"
           placeholder="Adicione um ingrediente"
           value={ingredient}
           onChange={handleInputChange}
         />
+
         <button onClick={handleAddIngredient}>+</button>
       </div>
-      <label>Dificuldade da receita</label>
-      <div className="button-toggle">
-      <button
-        className={activeButton === 0 ? 'button active' : 'button'}
-        onClick={() => handleButtonClick(0)}
-      >
-        Fácil
-      </button>
-      <button
-        className={activeButton === 1 ? 'button active' : 'button'}
-        onClick={() => handleButtonClick(1)}
-      >
-        Intermediaria
-      </button>
-      <button
-        className={activeButton === 2 ? 'button active' : 'button'}
-        onClick={() => handleButtonClick(2)}
-      >
-        Dificil
-      </button>
-    </div>
-    <div className="option-select">
-      <select value={selectedOption} onChange={handleChange}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
-    <div className="actions">
-      <Link  to='/Home'><button className='btnCancelar'>Cancelar</button></Link >
-      <button className='btnGerar'>Gerar</button>          
-    </div>
+      <div className='borda'>
+        <label className='nome-campo'>Dificuldade da receita</label>
+        <div className="button-toggle">
+          <button
+            className={activeButton === 0 ? 'button active' : 'button'}
+            onClick={() => handleButtonClick(0)}
+          >
+            Fácil
+          </button>
+          <button
+            className={activeButton === 1 ? 'button active' : 'button'}
+            onClick={() => handleButtonClick(1)}
+          >
+            Intermediaria
+          </button>
+          <button
+            className={activeButton === 2 ? 'button active' : 'button'}
+            onClick={() => handleButtonClick(2)}
+          >
+            Dificil
+          </button>
+        </div>
+      </div>
+      <label className='nome-campo'>Número de pessoas</label>
+      <div className="option-select">
+        <select value={selectedOption} onChange={handleChange}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="actions">
+        <Link to='/Home'><button className='btnCancelar'>Cancelar</button></Link >
+        <button className='btnGerar'>Gerar</button>
+      </div>
     </div>
 
   );
